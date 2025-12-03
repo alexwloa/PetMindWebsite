7d7b0bab6e1a05a85a2c578bf0eafd204206892f
Alexander Wimmer <alex0907w@gmail.com>
2025-12-03 12:17:08 +0100

    export bereit

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
