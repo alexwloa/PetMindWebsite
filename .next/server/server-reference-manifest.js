44774b446cc213eead5fc94f68d5e2ee7dca7600
Alexander Wimmer <alex0907w@gmail.com>
2025-12-03 12:17:08 +0100

    export bereit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/hWI4d9Ey8fGmrcCmZFV3AHWEabHitHPt/zLGm3zhOU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
